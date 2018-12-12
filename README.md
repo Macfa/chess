4a47e4d8262d710d71b37f153d7811642f7fb906
macfa <kombo6768@gmail.com>
2018-12-13 01:24:36 +0900

    download a data, and save it

--- README.md
+++ README.md
@@ -1,34 +1,33 @@
 # neural network Game by chy
 
 ## install
-* modules:\
-	* os\
-	* numpy\
+* modules:
+	* os
+	* numpy
 
 ## feature
-1. en passan:\
-	* 3, 6 rank or None\
-2. kingside castling:\
-	* R,K moves 2 step to each others.\
-3. queenside castling:\
-	* R,Q moves 2 step to each others.\
+1. en passan:
+	* 3, 6 rank or None
+2. kingside castling:
+	* R,K moves 2 step to each others
+3. queenside castling:
+	* R,Q moves 2 step to each others
 
 ## scenario
-
+* comm' soon
 
 ## files
-* parsing.py:\
-	* load a data file\
-	* push a moves in file to board\
-	* save a data\
+* parsing.py:
+	* load a data file
+	* push a moves in file to board
+	* save a data
 
-* state.py:\
-	* inialize a board\
-	* define a Method to use\
-	* could move list\
+* state.py:
+	* inialize a board
+	* define a Method to use
+	* could move list
 
-* play.py:\
-	* get a model's data\
-	* play ( WEB framework )\
-
+* play.py:
+	* get a model's data
+	* play ( WEB framework )
 
--- parsing.py
+++ parsing.py
@@ -1,21 +1,37 @@
 import chess.pgn
 import os
+import numpy as np
 
-file_list = os.listdir('./data')
+def save_data(data_limit=None):
+	file_list = os.listdir('./data')
+	score = 0
+	res_type = {'1-0':-1, '1/2-1/2':0, '0-1':1}
+	X,Y = [],[]
 
-for file in file_list:
-	pgn = open(os.path.join("./data", file))
-	# print(os.path.join("./data/", file))
-	game = chess.pgn.read_game(pgn)
+	for file in file_list:
+		pgn = open(os.path.join("./data", file))
+		while 1:
+			game = chess.pgn.read_game(pgn)
+			if game is None:
+				break
+			res = game.headers['Result']
+			if res not in res_type:
+				continue
+			
+			res = res_type[res]
+			score += 1
+			board = game.board()
 
-	# # Iterate through all moves and play them on a board.
-	board = game.board()
-	print(board)
-	# for move in :
-	# 	board.push(move)
-	
-	# print(game)
-	
-# first_game.headers["Event"]
+			for move in game.main_line():
+				pushed = board.push(move)
+				X.append(pushed)
+				Y.append(res)
+			print("%d Game, %d parsing" % (score, len(X)))
+			if data_limit is not None and len(X) > data_limit:
+				return X
+		return X
 
 
+if __name__ == "__main__":
+	X = save_data(30000)
+	np.savez('./proc/dataset.npz')
